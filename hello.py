172adab80832242b1490096bc0601a2fcd8deca1
Ismelka <ismelkatg@gmail.com>
2021-09-01 15:44:48 -0400

    cambio 5

--- hello.py
+++ hello.py
@@ -1,21 +1,18 @@
 print ("hello")
-ptint ("how are you")
-ptint ("I am fine ")
+print ("how are you")
+print ("I am fine ")
+
+def saludara() : 
+  print ("saludar cada que te saluden")
+
+saludara(); 
+
+nombre = "Ismelka"
+apellido="Tejada"
+
+def saludar (nombre, apellido) :
+  print("Buenos dias : " +apellido+ nombre)
 
 
-    def saludara (): 
-      print ("saludar cada que te saluden")
-
-
-      saludara (); 
-
-      def saludar (nombre, apellido):
-        print("Buenos dias" +apellido+ nombre + )
-
-
-        nombre = "Anabel"
-        apellido="Tejada"
-
-
-        saludar(nombre, apellido)
+saludar(nombre, apellido)
 
